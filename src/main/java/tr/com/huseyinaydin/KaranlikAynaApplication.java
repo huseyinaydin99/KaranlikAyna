70cf30fadfcfacc87c768a421c8e2aaf43b7b867
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-12 13:28:08 +0300

    Spring ve React projeleri oluşturuldu.

---
+++ src/main/java/tr/com/huseyinaydin/KaranlikAynaApplication.java
@@ -0,0 +1,12 @@
+package tr.com.huseyinaydin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaranlikAynaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaranlikAynaApplication.class, args);
+	}
+}
