75b1c5249dfb4819d1066c9e376595c1095a381c
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-13 15:13:07 +0300

    Vite config dosyası ile doğru adrese yönlendirme yapıldı.

--- src/main/java/tr/com/huseyinaydin/user/UserController.java
+++ src/main/java/tr/com/huseyinaydin/user/UserController.java
@@ -1,10 +1,13 @@
 package tr.com.huseyinaydin.user;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    //@CrossOrigin
     @PostMapping("/api/v1/users")
     public void createUser(){
 
