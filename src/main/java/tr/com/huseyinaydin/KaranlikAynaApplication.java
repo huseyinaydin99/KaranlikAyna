12ba8ddd3295b78fc2dbd03c00eab88f691de823
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-13 17:51:45 +0300

    Kullanıcı şifreleri database'de şifreli olarak saklandı.
    Kullanıcıya ait şifre Spring Security'nin BCryptPasswordEncoder sınıfı ile encode edildi. SQL Injection gibi saldırılarla veritabanı okunursa şifreler ele geçse bile artık şifreli oldukları için kötü niyetli amcaların işine yaramaz.

--- src/main/java/tr/com/huseyinaydin/KaranlikAynaApplication.java
+++ src/main/java/tr/com/huseyinaydin/KaranlikAynaApplication.java
@@ -2,8 +2,9 @@ package tr.com.huseyinaydin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class KaranlikAynaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/tr/com/huseyinaydin/user/UserService.java
+++ src/main/java/tr/com/huseyinaydin/user/UserService.java
@@ -1,6 +1,8 @@
 package tr.com.huseyinaydin.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,7 +10,10 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
