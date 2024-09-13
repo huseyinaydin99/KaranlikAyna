21b3138a163e0eb1bc668b18dadd23c8a5e90731
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-13 13:37:59 +0300

    UserController sınıfı yazıldı Axios Client ile React tarafından ilk istek gönderildi ve CORS hatası alındı.

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
+import axios from "axios";
 
 export function SignUp() {
     const [username, setUsername] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [passwordRepeat, setPasswordRepeat] = useState();
-    
+    const onSubmit = (event) => {
+      event.preventDefault(); //formdan gelen event'i alır ve tarayıcının işlmesini önler böylelikle form submit olunca tarayıcadaki sayfa yenilenmez.!
+      console.log(username + 
+        email + 
+        password);
+      axios.post('http://localhost:8080/api/v1/users',{
+        username,
+        email,
+        password
+    });
+    }
   return (
-    <>
+    <form onSubmit={onSubmit}>
       <h1>Kaydol</h1>
       <div>
         <label htmlFor="username">Kullanıcı Adı</label>
@@ -25,7 +36,7 @@ export function SignUp() {
         <label htmlFor="passwordRepeat">Şifre Tekrarı</label>
         <input id="passwordRepeat" type="password" onChange={(event) => {setPasswordRepeat(event.target.value);}} />
       </div>
-      <button disabled={!password && password !== passwordRepeat}>Kaydol</button>
-    </>
+      <button disabled={!password || password !== passwordRepeat}>Kaydol</button>
+    </form>
   );
 }
---
+++ src/main/java/tr/com/huseyinaydin/user/UserController.java
@@ -0,0 +1,12 @@
+package tr.com.huseyinaydin.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @PostMapping("/api/v1/users")
+    public void createUser(){
+
+    }
+}
