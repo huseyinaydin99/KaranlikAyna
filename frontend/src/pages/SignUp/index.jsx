d67bace8149553ad1d80fca5464e8f558f05e6d9
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-13 18:44:03 +0300

    Kayıt formu üzerinde tasarımsal düzenlemeler yapıldı.
    Bootstrap ve sass kütüphaneleri install edildi. styles.scss dosyasına bootstrap import edilip var olan özellikler override edildi. Kütüphanelerin komutları Vite ile REACT projesi oluşturma.txt dosyasına eklendi.

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,42 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
 
 export function SignUp() {
-    const [username, setUsername] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [passwordRepeat, setPasswordRepeat] = useState();
-    const onSubmit = (event) => {
-      event.preventDefault(); //formdan gelen event'i alır ve tarayıcının işlmesini önler böylelikle form submit olunca tarayıcadaki sayfa yenilenmez.!
-      console.log(username + 
-        email + 
-        password);
-      axios.post('http://localhost:8080/api/v1/users',{
-        username,
-        email,
-        password
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+  const onSubmit = (event) => {
+    event.preventDefault(); //formdan gelen event'i alır ve tarayıcının işlmesini önler böylelikle form submit olunca tarayıcadaki sayfa yenilenmez.!
+    console.log(username + email + password);
+    axios.post("http://localhost:8080/api/v1/users", {
+      username,
+      email,
+      password,
     });
-    }
+  };
   return (
-    <form onSubmit={onSubmit}>
-      <h1>Kaydol</h1>
-      <div>
-        <label htmlFor="username">Kullanıcı Adı</label>
-        <input id="username" type="text" onChange={(event) => {setUsername(event.target.value);}} />
+    <div className="container">
+      <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
+        <form className="card" onSubmit={onSubmit}>
+          <div className="text-center card-header">
+            <h1>Kaydol</h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Kullanıcı Adı
+              </label>
+              <input
+                id="username"
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                E-Posta
+              </label>
+              <input
+                id="email"
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Şifre
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="form-control"
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordRepeat" className="form-label">
+                Şifre Tekrarı
+              </label>
+              <input
+                id="passwordRepeat"
+                type="password"
+                className="form-control"
+                onChange={(event) => {
+                  setPasswordRepeat(event.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <button
+                disabled={!password || password !== passwordRepeat}
+                className="btn btn-primary">
+                Kaydol
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">E-Posta</label>
-        <input id="email" type="text" onChange={(event) => {setEmail(event.target.value);}} />
-      </div>
-      <div>
-        <label htmlFor="password">Şifre</label>
-        <input id="password" type="password" onChange={(event) => {setPassword(event.target.value);}} />
-      </div>
-      <div>
-        <label htmlFor="passwordRepeat">Şifre Tekrarı</label>
-        <input id="passwordRepeat" type="password" onChange={(event) => {setPasswordRepeat(event.target.value);}} />
-      </div>
-      <button disabled={!password || password !== passwordRepeat}>Kaydol</button>
-    </form>
+    </div>
   );
 }
