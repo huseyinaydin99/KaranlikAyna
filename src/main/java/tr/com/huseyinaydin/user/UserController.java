44faa918842275f10f16f1230df3d3509e31e6bc
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-14 17:33:35 +0300

    Kullanıcı doğrulaması yapıldı.
    Backend tarafında UserController'da kullanıcı adı ve şifresi boş gelen kullanıcı için ApiError sınıfı oluşturuldu. Hangi adrese istek atıldıysa o adres için 400 bad request mesajı dönüldü.

--- src/main/java/tr/com/huseyinaydin/user/UserController.java
+++ src/main/java/tr/com/huseyinaydin/user/UserController.java
@@ -1,12 +1,17 @@
 package tr.com.huseyinaydin.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import tr.com.huseyinaydin.error.ApiError;
 import tr.com.huseyinaydin.shared.GenericMessage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -16,8 +21,18 @@ public class UserController {
 
     @CrossOrigin
     @PostMapping("/api/v1/users")
-    public GenericMessage createUser(@RequestBody User user){
+    public ResponseEntity<?> createUser(@RequestBody User user){
+        if(user.getUsername() == null || user.getUsername().isEmpty()){
+            ApiError apiError = new ApiError();
+            apiError.setPath("/api/v1/users");
+            apiError.setMessage("Doğrulama hatası");
+            apiError.setStatus(400);
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username", "Kullanıcı adı boş olamaz.");
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.badRequest().body(apiError);
+        }
         userService.save(user);
-        return new GenericMessage("Kullanıcı oluşturuldu.");
+        return ResponseEntity.ok(new GenericMessage("Kullanıcı oluşturuldu"));
     }
 }
