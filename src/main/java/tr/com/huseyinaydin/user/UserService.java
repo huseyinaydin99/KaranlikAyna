93643227829ea293da8679673cd72ae64535a286
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-23 11:38:28 +0300

    Kullanıcı kaydı sonrası aktivasyon maili gönderme işlemleri yapıldı.
    UserService içerisinde ethereal.email servisi kullanılarak  e-posta gönderme metotları yazıldı. E-posta göndermek için Spring'in kendi sınıfları kullanıldı.

--- src/main/java/tr/com/huseyinaydin/user/UserService.java
+++ src/main/java/tr/com/huseyinaydin/user/UserService.java
@@ -1,8 +1,12 @@
 package tr.com.huseyinaydin.user;
 
 import java.util.UUID;
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +27,25 @@ public class UserService {
         catch(DataIntegrityViolationException ex){
 
         }
-        
+    }
+
+    private void sendActivationEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@my-app.com");
+        message.setTo(user.getEmail());
+        message.setSubject("Hesap Aktivasyonu");
+        message.setText("http://localhost:5173/activation/" + user.getActivationToken());
+        getJavaMailSender().send(message);
+    }
+
+    public JavaMailSender getJavaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.ethereal.email");
+        mailSender.setPort(587);
+        mailSender.setUsername("deon.williamson23@ethereal.email");
+        mailSender.setPassword("FPC8Rgpy5A9n7Wk1jf");
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.smtp.starttls.enable", "true");
+        return mailSender;
     }
 }
