88717c663ca954f99087b33704db527958e48d3b
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-24 15:37:03 +0300

    React tarafında sayfalama yapıldı.
    Home component'in içindeki api.js dosyasında loadUsers fonksiyonu api'ye get isteği atıyor. İstek atılırken obje içerisinde page ve size parametreleri giriliyor. page o an hangi sayfadaysak o. size ise her sayfada kaç kaydın görüntülenebileceğini verir.

--- frontend/src/pages/Home/components/UserList.jsx
+++ frontend/src/pages/Home/components/UserList.jsx
@@ -1,23 +1,39 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { loadUsers } from "./api";
 
 export function UserList() {
-  const [users, setUsers] = useState([]);
+  const [userPage, setUserPage] = useState({
+    content: [],
+    last: false,
+    first: false,
+    number: 0,
+  });
 
-  useEffect(() => {
+  /*useEffect(() => {
     async function getUsers() {
       const response = await loadUsers();
       setUsers(response.data);
     }
+    getUsers();
+  }, []);*/
+
+  const getUsers = useCallback(async (page) => {
+    const response = await loadUsers(page);
+    setUserPage(response.data);
+  }, []);
+
+  useEffect(() => {
     getUsers();
   }, []);
 
   return (
     <>
       <div>Kullanıcı Listesi</div>
-      {users.map((user) => {
+      {userPage.content.map((user) => {
         return <div>{user.username}</div>;
       })}
+      {!userPage.first && <button onClick={() => getUsers(userPage.number - 1)}>Previous</button>}
+      {!userPage.last && <button onClick={() => getUsers(userPage.number + 1)}>Next</button>}
     </>
   );
 }
