93b2416b4ae7214743ed5da7a8bcafbb49d01a7e
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-09-24 14:17:06 +0300

    Database'e kullanıcılar insert edildi.
    CommandLineRunner interface'ini implemente eden bir sınıfı Spring otomatik olarak var ediyor. Bunun içinde bir tane metot var. Bu metoda biz döngü ile kullanıcılar kaydediyoruz. Spring'in IoC container'i bunu otomatik çalıştırıp database'e save ediyor. @Profile("dev") dipnotu sayesinde sadece geliştirme modundayken metot devreye giriyor.

--- src/main/java/tr/com/huseyinaydin/KaranlikAynaApplication.java
+++ src/main/java/tr/com/huseyinaydin/KaranlikAynaApplication.java
@@ -1,13 +1,37 @@
 package tr.com.huseyinaydin;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import tr.com.huseyinaydin.user.User;
+import tr.com.huseyinaydin.user.UserRepository;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class KaranlikAynaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KaranlikAynaApplication.class, args);
+	}
+
+	@Bean
+	@Profile("dev")
+	public CommandLineRunner userCreator(UserRepository userRepository) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return (args) -> {
+			for (var i = 1; i <= 25; i++) {
+				User user = new User();
+				user.setUsername("user" + i);
+				user.setEmail("user" + i + "@mail.com");
+				user.setPassword(passwordEncoder.encode("P4ssword"));
+				user.setActive(true);
+				userRepository.save(user);
+			}
+		};
 	}
 }
