6a0634ba8a13679309097c2a0b9dc1d9335e31ac
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-10-18 12:28:29 +0300

    React çıkış(logout) işlemi yapıldı.
    React tarafında logout işlemi context.js'deki onLogoutSuccess fonksiyonu ile tarayıcının deposundan kaldırıldı. onLogoutSuccess context yapısı tarafından alt komponentlere state management ile geçildi.

--- frontend/src/shared/components/NavBar.jsx
+++ frontend/src/shared/components/NavBar.jsx
@@ -6,6 +6,11 @@ import { useContext } from "react";
 export function NavBar() {
   const { t } = useTranslation();
   const authState = useContext(AuthContext)
+
+  const onClickLogout = () => {
+    authState.onLogoutSuccess();
+  }
+
   return (
     <nav className="navbar navbar-expand bg-body-tertiary shadow-sm">
       <div className="container-fluid">
@@ -36,7 +41,7 @@ export function NavBar() {
                 </Link>
               </li>
               <li className="nav-item">
-                <span className="nav-link" role="button">Çıkış</span>
+                <span className="nav-link" role="button" onClick={onClickLogout}>Çıkış</span>
               </li>
             </>
           )}
