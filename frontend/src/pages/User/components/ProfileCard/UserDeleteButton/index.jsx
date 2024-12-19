02e036365cd44ba0a826875a741f4da33e0e12d1
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-19 16:29:33 +0300

    React/istemci tarafı kullanıcı silme işlemleri ve silme butonu komponenti.
    Kullanıcıyı silmek için bir UserDeleteButton komponenti oluşturdum ve API entegrasyonunu tamamladım. Silme işlemi sırasında kullanıcıdan onay almak için confirm penceresi kullandım. Eğer kullanıcı silme işlemini onaylarsa, deleteUser API çağrısını yaparak backend ile iletişim kuruyorum. İşlem tamamlandığında kullanıcıyı oturumdan çıkarıp ana sayfaya yönlendiren bir yapı ekledim. Ayrıca, butonun durumu için apiProgress ile işlem sırasında kullanıcıya görsel geri bildirim sağladım.

---
+++ frontend/src/pages/User/components/ProfileCard/UserDeleteButton/index.jsx
@@ -0,0 +1,7 @@
+import { Button } from "@/shared/components/Button";
+import { useUserDeleteButton } from "./useUserDeleteButton";
+
+export function UserDeleteButton(){
+    const {apiProgress, onClick} = useUserDeleteButton();
+    return <Button styleType="danger" apiProgress={apiProgress} onClick={onClick}>Sil</Button>
+}
--- frontend/src/pages/User/components/ProfileCard/index.jsx
+++ frontend/src/pages/User/components/ProfileCard/index.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/shared/components/Button";
 import { useAuthState } from "@/shared/state/context";
 import { UserEditForm } from "./UserEditForm";
+import { UserDeleteButton } from "./UserDeleteButton";
 
 export function ProfileCard({ user }) {
   //const authState = useContext(AuthContext);
@@ -10,21 +11,29 @@ export function ProfileCard({ user }) {
   const [editMode, setEditMode] = useState(false);
   const [tempImage, setTempImage] = useState();
 
-  const isEditButtonVisible = !editMode && authState.id === user.id;
+  //const isEditButtonVisible = !editMode && authState.id === user.id;
   const visibleUsername =
     authState.id === user.id ? authState.username : user.username;
+
+  const isLoggedInUser = !editMode && authState.id === user.id;
 
   return (
     <div className="card">
       <div className="card-header text-center">
-        <ProfileImage width={200} tempImage={tempImage} image={user.image}/>
+        <ProfileImage width={200} tempImage={tempImage} image={user.image} />
       </div>
       <div className="card-body text-center">
         {!editMode && <span className="fs-3 d-block">{visibleUsername}</span>}
-        {isEditButtonVisible && (
-          <Button onClick={() => setEditMode(true)}>Edit</Button>
+        {isLoggedInUser && (
+          <>
+            <Button onClick={() => setEditMode(true)}>Düzenle</Button>
+            <div className="d-inline m-1"></div>
+            <UserDeleteButton />
+          </>
         )}
-        {editMode && <UserEditForm setEditMode={setEditMode} setTempImage={setTempImage}/>}
+        {editMode && (
+          <UserEditForm setEditMode={setEditMode} setTempImage={setTempImage} />
+        )}
       </div>
     </div>
   );
